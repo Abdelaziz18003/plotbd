855a9d1bbd7eed7024567e7164dbfbd6be9661d7
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2019-06-15 11:34:32 +0100

    generate tempDataFile

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const tempDataFile = '.bifurcation-data.dat';
+const defaultOptions = {
+  x0: 0.4,
+  rValues: [0.1, 3.99, 1000],
+  iterations: 300,
+  density: 100
+}
+
+function plotbd (chaoticMap, {x0, rValues, iterations, density} = defaultOptions) {
+  let dataFile = fs.createWriteStream(`${tempDataFile}`);
+  const [rMin, rMax, rNum] = rValues;
+  iterations = iterations ? iterations : defaultOptions.iterations;
+  density = density ? density : defaultOptions.density;
+
+  for (let r = rMin; r <= rMax; r = r + (rMax - rMin) / rNum) {
+    let x = x0;
+    for (let i = 0; i < iterations; i++) {
+      x = chaoticMap(x, r);
+      if (i >= iterations - density) {
+        dataFile.write(`${r} ${x}\n`);
+      }
+    }
+  }
+  dataFile.end();
+}
+
+module.exports = plotbd;
